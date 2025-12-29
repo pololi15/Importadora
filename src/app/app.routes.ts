1475cf138fa4a5ecb3dc5ce403482ce828ced080
pololi15 <ostinrivas15@gmail.com>
2025-12-28 23:29:35 -0400

    Disenios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { Home } from './pages/home/home';
+import { HomeComponent } from './pages/home/home';
 import { ProductsComponent } from './pages/products/products';
 //import { Products } from './pages/products/products';
 import { About } from './pages/about/about';
 
 export const routes: Routes = [
-    { path: '', component: Home },
+    { path: '', component: HomeComponent },
     { path: 'productos', component: ProductsComponent },
     //{ path: 'productos', component: Products },
     { path: 'nosotros', component: About },
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Navbar } from './layout/navbar/navbar';
+import { NavbarComponent } from './layout/navbar/navbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, NavbarComponent],
   template: `
     <app-navbar />
     <main class="max-w-7xl mx-auto px-4 py-6">
