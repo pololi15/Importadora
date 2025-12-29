794967aabc2b9d1e1b8a4e309084759c4226c6bb
pololi15 <ostinrivas15@gmail.com>
2025-12-29 00:43:51 -0400

    style: estilos en about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home';
 import { ProductsComponent } from './pages/products/products';
 //import { Products } from './pages/products/products';
-import { About } from './pages/about/about';
+import { AboutComponent } from './pages/about/about';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'productos', component: ProductsComponent },
     //{ path: 'productos', component: Products },
-    { path: 'nosotros', component: About },
+    { path: 'nosotros', component: AboutComponent },
     { path: '**', redirectTo: '' }
 ];
--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './about.html',
-  //styleUrl: './about.css',
 })
-export class About {
-
-}
+export class AboutComponent {}
