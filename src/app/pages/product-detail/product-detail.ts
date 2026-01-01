bf681523fd437b1f4bd465e509d617a1770c0b76
pololi15 <ostinrivas15@gmail.com>
2026-01-01 03:07:45 -0400

    feat: conexion con whatsapp Final

--- src/app/pages/product-detail/product-detail.ts
+++ src/app/pages/product-detail/product-detail.ts
@@ -4,14 +4,15 @@ import { ActivatedRoute } from '@angular/router';
 import { Products } from '../../services/products';
 import { Product } from '../../models/product.model';
 import { ChangeDetectorRef } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 
 @Component({
   selector: 'app-product-detail',
-  standalone: true,                 // 👈 standalone
-  imports: [CommonModule],          // 👈 directivas comunes (*ngIf, *ngFor)
+  standalone: true,                 
+  imports: [CommonModule],          
   templateUrl: './product-detail.html',
-  styleUrls: ['./product-detail.css'], // 👈 plural
+  styleUrls: ['./product-detail.css'], 
 })
 export class ProductDetailComponent implements OnInit {
   product!: Product;
@@ -27,7 +28,7 @@ export class ProductDetailComponent implements OnInit {
   const id = Number(this.route.snapshot.paramMap.get('id'));
 
   this.loading = true;
-  this.cdr.detectChanges(); // 👈 CLAVE
+  this.cdr.detectChanges(); 
 
   try {
     this.product = await this.productsService.getProductById(id);
@@ -35,7 +36,15 @@ export class ProductDetailComponent implements OnInit {
     console.error(error);
   } finally {
     this.loading = false;
-    this.cdr.detectChanges(); // 👈 CLAVE
+    this.cdr.detectChanges(); 
     }
   }
+  get whatsappLink(): string {
+  if (!this.product) return '';
+
+  const message = `Hola, estoy interesado en el producto "${this.product.name}" que cuesta Bs ${this.product.price_b2c}. ¿Me podrías dar más información?`;
+
+  return `https://wa.me/${environment.whatsappNumber}?text=${encodeURIComponent(message)}`;
+}
+
 }
