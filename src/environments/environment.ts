769e0faaf1dba367ae395376b9595ed449158854
pololi15 <ostinrivas15@gmail.com>
2026-09-06 23:08:55 -0400

    Remover environment.ts del repositorio y mantenerlo local

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://kmmjjplvafpbkuesolui.supabase.co',
-  supabaseAnonKey: 'sb_publishable_ZM_QerBMi4vMv_JzSvgTNA_hc1OM4B4',
-  whatsappNumber: '59165707358'
-};
